4b7877782a152cb6c687447c1aca4c171ef17e32
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-09-30 09:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Get references to the elements
+const statusElement = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+// Add a click event listener to the button
+enterBtn.addEventListener("click", function() {
+    // Change the text of the <p> tag to "Entered Metaverse"
+    statusElement.innerHTML = "<h1>Entered Metaverse</h1>";
+});
